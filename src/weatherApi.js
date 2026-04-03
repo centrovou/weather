2e2a47751881188ee24c74dcbcf2355b5faa721a
centrovou <120648650+centrovou@users.noreply.github.com>
2026-04-03 15:59:33 +0300

    Update weatherApi.js

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -6,7 +6,7 @@ const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 export const WEATHER_API = axios.create({
   baseURL: API_ENDPOINT,
   params: {
-    key: API_KEY,
+    key: '99774110a3a7432faa7112131260204',
     lang: 'ru',
   },
 });
